945afea72c80126e42a25d53a5252fd6530a3680
ksbminfotech5685 <m8899021313@gmail.com>
2025-10-21 15:45:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
+{
+  "name": "astroone-realtime-server",
+  "type": "module",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "node-fetch": "^3.4.0",
+    "dotenv": "^16.0.0",
+    "body-parser": "^1.20.2"
+  }
+}
 
